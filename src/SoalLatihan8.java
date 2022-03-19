7e1da917a53481e200368ce210c50cbdd4437fff
CheesyStars <sigitkurniawanmtsn@gmail.com>
2022-03-19 08:42:00 +0700

    Praktikum 3 Dasar Pemrograman Java Bagian - III
    Mahasiswa mengenal dan dapat mengimplementasikan daripada fungsi math, array, String dan collection (arraylist dan hashmap) dalam sebuah program

---
+++ src/SoalLatihan8.java
@@ -0,0 +1,25 @@
+//Mengimport Dari Library Java
+import java.util.Scanner;
+
+public class SoalLatihan8 {
+    //Membuat Class
+    static char[] swap(String mystring, int i, int j) {
+        char ch[] = mystring.toCharArray();
+        char tempo = ch[i];
+        ch[i] = ch[j];
+        ch[j] = tempo;
+        return ch;
+    }
+
+    public static void main(String[] args) {
+        //Membuat Variabel Dan Scanner
+        Scanner scan = new Scanner(System.in);
+        int i,j;
+
+        //Mengambil Inputan Dari User
+        System.out.print("Masukkan Kalimat : ");
+        String kalimat = scan.nextLine();
+
+        System.out.println(swap(kalimat,0,kalimat.length()-1));
+    }
+}
